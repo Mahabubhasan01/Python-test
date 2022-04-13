b5438e09ff5c2ae2d86238231d2f25dc8b7cf3cf
Mahabubhasan01 <mahabubhasaan@outlook.com>
2022-04-13 14:25:51 +0600

    math library

---
+++ mathLibarary.py
@@ -0,0 +1,45 @@
+
+#Find maximum value for using python built in max() function
+import math
+from math import *
+max_val= max(100,12,123,10)
+print(max_val)
+x=4
+y=2
+print(max(x,y))
+
+# find ceil value using math.ceil() function
+k = 77.4  # return 78
+va = math.ceil(k)
+print(va)
+
+# using math.floor() function
+m = 5.45
+value = math.floor(m)
+print(value)
+# if you write negative positive you can convert absolute value number
+h = -35
+ok = math.fabs(h)
+print(ok)
+# total sum using list
+total = math.fsum([100,200,300,400,500])
+print(total)
+
+#
+com = math.comb(5,3)
+print(com)
+# power or squire
+power = math.pow(4,4)
+print(power)
+
+# Round function
+
+ab = round(3,22)
+print(ab)
+
+# root or squire root find
+root = sqrt(2500)
+print(root)
+
+pi = math.pi
+print(pi)
