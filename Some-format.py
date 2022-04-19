f352988c87752e627a6e51c415e6bfa1affa3913
Mahabubhasan01 <mahabubhasaan@outlook.com>
2022-04-19 12:29:54 +0600

    break and continue impliment

--- Some-format.py
+++ Some-format.py
@@ -9,3 +9,38 @@ name = 'sanvi rahman age 22'
 print(name)
 
 # 19/4/22
+
+# if you using while loop and wanna broke this loop firstle and also can use break method .
+i = 1
+while i<=100:
+    if i==30:
+        break
+    print('i love python',i)
+    i+=1
+
+
+# second format
+i = 1
+while i<=100:
+    print('i love my country',i)
+    i+=1
+    if i==50:
+        break
+
+
+# if you using while loop and wanna continue with conditionaly like i==20 then continue this loop firstly and also can use continue method .
+i = 1
+while i<=100:
+    if i==30:
+        continue
+    print('i love python with continue',i)
+    i+=1
+
+
+# second format
+i = 1
+while i<=100:
+    print('i love my country with continue',i)
+    i+=1
+    if i==50:
+        continue
